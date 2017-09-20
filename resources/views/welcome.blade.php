aca58b3f3ba216cf0cf8ec04851208f4982f570f
iAmBentley <joey@moticorp.com>
2017-09-20 16:08:42 -0400

    Small welcome.blade change for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Powders</a>
+                    <a href="https://laracasts.com">Capsules-Powder</a>
+                    <a href="https://laravel-news.com">Capsules-Extracts</a>
+                    <a href="https://forge.laravel.com">Extracts</a>
+                    <a href="https://github.com/laravel/laravel">Wholesale</a>
                 </div>
             </div>
         </div>
