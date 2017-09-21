a970d2c0098c8d5a22bbb200ae07b62a300b6a89
iAmBentley <joey@moticorp.com>
2017-09-21 10:44:55 -0400

    Structure layouts

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,24 @@
+{{--========== Javascript Files ==========--}}
+    {{-- jQuery Latest --}}
+    <script src="/js/lib/jquery-2.2.0.min.js"></script>
+
+    {{-- Bootstrap JS --}}
+    <script src="/js/lib/bootstrap.min.js"></script>
+
+    {{-- Plugins --}}
+    <script src="/vendors/owl-carousel/owl.carousel.js"></script>
+    <script src="/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+    <script src="/vendors/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="/js/plugins/gmaps.min.js"></script>
+    <script src="/js/plugins/google-map.js"></script>
+    <script src="/js/plugins/wow.min.js"></script>
+    <script src="/js/plugins/validate.js"></script>
+    {{-- Includes --}}
+    <script src="/js/includes/pre-order.js"></script>
+    <script src="/js/includes/subscribe.js"></script>
+    <script src="/js/includes/contact.js"></script>
+
+    {{-- Main JS --}}
+    <script src="/js/main.js"></script>
