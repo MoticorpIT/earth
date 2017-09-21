723c6b34fac8e412ac8cb8f1a1d1a157d64a7228
iAmBentley <joey@moticorp.com>
2017-09-21 10:44:55 -0400

    Structure layouts

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        {{--========== Brand and toggle get grouped for better mobile display ==========--}}
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#header"><img src="/images/theme/logo-white.png" class="white-logo" alt=""><img src="/images/theme/logo.png" class="dark-logo" alt=""></a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        {{--========== Collect the nav links, forms, and other content for toggling ==========--}}
+        <div class="collapse navbar-collapse" id="main-navbar">
+            <a href="http://indegogo.com/" class=" btn btn-warning pull-right hidden-sm hidden-xs">Back this project</a>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#product">Product</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#reviews">Reviews</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="index.html">Pre-order (Email)</a></li>
+                        <li><a href="index_email.html">Email Signup (Mailchimp)</a></li>
+                        <li><a href="index_buy_paypal.html">Buy Now (PayPal)</a></li>
+                        <li><a href="index_kickstarter.html">Kickstarter Campaign</a></li>
+                        <li><a href="index_indegogo.html">Indegogo Campaign</a></li>
+                        <li><a href="index_boxed.html">Boxed Version</a></li>
+                        <li><a href="index_rtl.html">RTL Version</a></li>
+                        <li><a href="index_video.html">Video Background</a></li>
+                        <li class="divider"></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="blog-single.html">Blog Single</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="faq.html">FAQ</a></li>
+                        <li><a href="shortcodes.html">Shortcodes</a></li>
+                    </ul>
+                </li>
+                <li><a href="#contact">contact</a></li>
+            </ul>
+        </div>
+        {{--========== /.navbar-collapse ==========--}}
+    </div>
+    {{--========== /.container-fluid ==========--}}
+</nav>
